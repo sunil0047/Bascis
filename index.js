b8885825d2f146b55acb8bb105af891e950e7788
Sunil Singh 3 <ssi276@sapient.com>
2018-03-13 14:45:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function v(val){
+    document.getElementById("d").value+=val;
+  }
+
+  function e(){
+     console.log(c(eval(document.getElementById("d").value)));
+  }
+  function c(val){
+      document.getElementById("d").value=val;
+  }
